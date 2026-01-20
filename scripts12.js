92f3471499e52bd21b46c833373c67a46f9e98ed
ZipZhu <153529914+ZipZhu@users.noreply.github.com>
2026-01-20 19:33:26 +0800

    Update filterKeywords regex to prevent matching '集'

--- scripts12.js
+++ scripts12.js
@@ -5,7 +5,7 @@ function main(config) {
   const maxRatio = 3.0;
   const ratioRegex = /(?:\[(\d+(?:\.\d+)?)\s*(?:x|X|×)\]|(\d+(?:\.\d+)?)\s*(?:x|X|×|倍)|(?:x|X|×|倍)\s*(\d+(?:\.\d+)?))/i;
 
-  const filterKeywords = '群|邀请|返利|官网|官方|网址|订阅|购买|续费|剩余|到期|过期|流量|备用|邮箱|客服|联系|工单|倒卖|防止|梯子|tg|发布|重置';
+  const filterKeywords = '(?<!集)群|邀请|返利|官方|网址|订阅|购买|续费|剩余|到期|过期|流量|备用|邮箱|客服|联系|工单|倒卖|防止|梯子|tg|发布|重置';
   const blackListRegex = new RegExp(filterKeywords);
 
   function normalizeName(name = '') {
